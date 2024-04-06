cdc9024f312558072d8ecb5e24a6ed59c61991f9
bagaretk <sebastianistrate26@gmail.com>
2024-04-06 17:33:08 +0300

    first commit

---
+++ lvl_1_gun.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+func _physics_process(delta):
+	var enemiesInRange = get_overlapping_bodies()
+	if enemiesInRange.size() > 0:
+		var target_enemy = enemiesInRange.front()
+		look_at(target_enemy.global_position)
+
+func shoot():
+	#load() le incarca mereu cand e nevoie
+	#preload() le incarca la inceputu jocului, static, absolut path
+	const BULLET = preload("res://laserAlbastru.tscn")
+	var newBullet = BULLET.instantiate()
+	newBullet.global_position = %ShootingPoint.global_position
